a6af1aa21372633ee7a327aaba717a4524769bbf
LEEBUYONG <dqd001@naver.com>
2025-03-27 14:04:17 +0900

    feat: UI 코드베이스 작성 완료

--- CounterCodeBaseUI/CounterCodeBaseUI/ViewController.swift
+++ CounterCodeBaseUI/CounterCodeBaseUI/ViewController.swift
@@ -11,16 +11,54 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    var number: Int = 0
     let countLabel = UILabel()
-    let downButton1 = UIButton()
+    let downButton = UIButton()
     let upButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .black
         
+        [countLabel, upButton, downButton].forEach { view.addSubview($0)}
         
+        countLabel.text = "\(number)"
+        countLabel.textColor = .white
+        countLabel.font = UIFont.boldSystemFont(ofSize: 45)
+        countLabel.textAlignment = .center
         
+        countLabel.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.center.equalToSuperview()
+        }
         
+        upButton.setTitle("증가", for: .normal)
+        upButton.backgroundColor = .blue
+        upButton.layer.cornerRadius = 8
+        upButton.titleLabel?.textColor = .white
+        
+        upButton.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(30)
+            $0.centerY.equalTo(countLabel.snp.centerY)
+            $0.leading.equalTo(countLabel.snp.trailing).offset(32)
+        }
+        
+        downButton.setTitle("감소", for: .normal)
+        downButton.backgroundColor = .red
+        downButton.layer.cornerRadius = 8
+        downButton.titleLabel?.textColor = .white
+        
+        downButton.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(30)
+            $0.centerY.equalTo(countLabel.snp.centerY)
+            $0.trailing.equalTo(countLabel.snp.leading).offset(-32)
+        }
     }
 
 
